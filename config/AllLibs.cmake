8dbf02577f5a9869d0c94490d8ea6f98591bdccd
Alex Robenko <arobenko@gmail.com>
2024-10-18 07:48:43 +1000

    Fixing spelling typo in configuraiton files.

--- config/AllLibs.cmake
+++ config/AllLibs.cmake
@@ -1,9 +1,9 @@
-macro (enalbe_if_opt_not_set name)
+macro (enable_if_opt_not_set name)
     if (NOT DEFINED ${name})
         set (${name} ON)
     endif ()
 endmacro()
 
-enalbe_if_opt_not_set (OPT_CC_MQTTSN_LIBS)
-enalbe_if_opt_not_set (OPT_CC_MQTT311_LIBS)
-enalbe_if_opt_not_set (OPT_CC_MQTT5_LIBS)
+enable_if_opt_not_set (OPT_CC_MQTTSN_LIBS)
+enable_if_opt_not_set (OPT_CC_MQTT311_LIBS)
+enable_if_opt_not_set (OPT_CC_MQTT5_LIBS)
